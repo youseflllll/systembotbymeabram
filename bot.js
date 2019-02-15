c590a1972a5e7a16368df7b008ec30920bbb49f7
youseflllll <47567110+youseflllll@users.noreply.github.com>
2019-02-15 22:50:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ var ti={}
 
 client.on('ready', function(){
     var ms = 3000 ;
-    var setGame = ['$help','Bot By ZEGO'];
+    var setGame = ['$help','𝔽ℝ𝕀𝔼ℕ𝔻𝕊 ♥'];
     var i = -1;
     var j = 0;
     setInterval(function (){
