527fbabdcaf7dc7121b5d696811db9ce517edb1a
youseflllll <47567110+youseflllll@users.noreply.github.com>
2019-02-16 03:09:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3651,8 +3651,9 @@ client.on("message", async message => {
             message.channel.sendEmbed(invite);
     }
 });
+
 client.on("message", async message => {
-    if(message.content.startsWith(prefix + "help-bc")) {
+    if(message.content.startsWith(prefix + "help")) {
         let help = new Discord.RichEmbed()
             .setColor("RANDOM")
             .setThumbnail(message.author.avatarURL)
@@ -3702,33 +3703,5 @@ client.on("message", message => { //clear
        
 });
 
-const moment = require('moment');//npm install moment
-client.on('message', message => {
-             var prefix = "$"
-if (message.content.startsWith(prefix + "time")) {
-let user = message.mentions.users.first();
-var args = message.content.split(" ").slice(1);
-var men = message.mentions.users.first();
-var heg;
-if(men) {
-heg = men
-} else {
-heg = message.author
-}
-var mentionned = message.mentions.members.first();
-var h;
-if(mentionned) {
-h = mentionned
-} else {
-h = message.member
-}
-moment.locale('ar-TN'); //TN
-var id = new  Discord.RichEmbed()
-.setColor("RANDOM")
-.setAuthor(`${heg.username}#${heg.discriminator} `,heg.avatarURL)
-.setDescription([`**الوقت**${moment().format('HH:mm:ss A')}${moment().format('YYYY/M/D')}**اليوم** :${moment().format('dddd')}**التاريخ**`])
-message.channel.send(546133977912705045)
-};
-});
 
 client.login("NTQzODQ4MDM4MDk2MTc1MTQ1.D0i6rA.xE4CIhQH5NuAkKK98j38EXFxzR0")
